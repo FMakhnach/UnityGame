5e409b7e4368ffc28bbddab578400c524b885552
FMakhnach <55485338+FMakhnach@users.noreply.github.com>
2020-04-06 01:20:23 +0300

    Day 1
    - Hexagonal grid;
    - Basic unit path movement;
    - Primitive interations (spawning units and placing towers);

---
+++ Assets/DevOnly/TilesGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Just for easy generating grid while developing.
+/// </summary>
+public class TilesGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private Tile tilePrefab;
+    [SerializeField]
+    private Spawn spawnPrefab;
+    [SerializeField]
+    private RoadConfiguration road;
+
+    private void Awake()
+    {
+        GameObject tileParent = new GameObject("Tiles");
+
+        Tile[,] tiles = new Tile[10, 10];
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Tile tile = Instantiate(tilePrefab, tileParent.transform);
+                tile.PlaceOnMap(i, j);
+                tile.name = tile.ToString();
+                tiles[i, j] = tile;
+            }
+        }
+
+        // Creating a path of tiles.
+        var path = new Tile[road.coordinates.Length];
+        for (int i = 0; i < path.Length; i++)
+        {
+            path[i] = tiles[road.coordinates[i].x, road.coordinates[i].y].GetComponentInParent<Tile>();
+        }
+        // Creating a spawn object with path.
+        Spawn spawn = Instantiate(spawnPrefab, path[0].GetComponentInChildren<MeshCollider>().transform);
+        spawn.Path = path;
+        path[0].isOccupied = true;
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Represents a hexagonal tile.
+/// </summary>
+[RequireComponent(typeof(Collider))]
+public class Tile : MonoBehaviour
+{
+    private int x;
+    private int y;
+    private TileContent content;
+
+    [HideInInspector]
+    public bool isOccupied;
+
+    // Magic coordinate system.
+    public int X => x;
+    public int Y => y;
+
+    public void PlaceOnMap(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        transform.position = new Vector3(1.5f * x, 0, (1.73205f * y) + 0.86602f * (x % 2));
+    }
+
+    public bool TryToPlace(TileContent prefab)
+    {
+        if (isOccupied)
+        {
+            return false;
+        }
+        else
+        {
+            content = Instantiate(prefab);
+            Vector3 pos = this.transform.position;
+            // Need 0.2 to place obj above the tile.
+            pos.y += 0.2f ;
+            content.transform.position = pos;
+            isOccupied = true;
+            return true;
+        }
+    }
+
+    // FOR DEBUGGING
+    public override string ToString()
+        => "(" + x + ", " + y + ")";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Unit unit = other.GetComponent<Unit>();
+        if (unit is PlayerUnit)
+        {
+            unit.UpdateDestination(this);
+        }
+        else if (unit is EnemyUnit)
+        {
+            unit.UpdateDestination(this);
+        }
+    }
+}
